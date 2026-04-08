0f32d5b118636b877fb5876d8f9d2cae61ad84ec
wallcreation <lanhajames@gmail.com>
2026-04-08 16:17:55 +0100

    Rename file

--- app/pages/businesses/index.vue
+++ app/pages/businesses/index.vue
@@ -182,7 +182,7 @@ const navigateToEdit = (id: string) => {
 
 .line-clamp-2 {
   display: -webkit-box;
-  -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
